c3c99aa212e3462f7c645d0b4b5c1af423f1591d
madaanp <73661923+madaanp@users.noreply.github.com>
2020-11-25 20:36:39 +0530

    update

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,7 @@
 #########################################
 # Installing python and necessary packages
 #########################################
-mkdir madaanp
-python3 -m venv madaanp/
-source madaanp/bin/activate
-pip install numpy
-pip install pillow
-pip install CaptchaSolving_RPi/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
+pip3 install numpy
+pip3 install pillow
+pip3 install CaptchaSolving_RPi/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
 python3 CaptchaSolving_RPi/test.py --model CaptchaSolving_RPi/tflitemodel.tflite --inputDir CaptchaSolving_RPi/MADAANP-project2rpi/ --output result.txt
