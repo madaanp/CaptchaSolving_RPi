28d810401359bd20030f9916c96a629545d6db2f
madaanp <73661923+madaanp@users.noreply.github.com>
2020-11-25 21:02:09 +0530

    Update

--- requirements.sh
+++ requirements.sh
@@ -4,4 +4,3 @@
 pip3 install numpy
 pip3 install pillow
 pip3 install CaptchaSolving_RPi/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
-python3 CaptchaSolving_RPi/test.py --model CaptchaSolving_RPi/tflitemodel.tflite --inputDir CaptchaSolving_RPi/MADAANP-project2rpi/ --output result.txt
